9eb560e736f78090121101a37092978358f991f9
chison2707 <chiphansonzz17@gmail.com>
2025-03-26 16:30:21 +0700

    tạo config vnpay

---
+++ config/vnpay.js
@@ -0,0 +1,15 @@
+import { VNPay, ignoreLogger } from 'vnpay';
+
+const vnpay = new VNPay({
+    // Thông tin cấu hình bắt buộc
+    tmnCode: 'M5UIQKTP',
+    secureSecret: 'GJGLD64LCT6X3SDKH8LD7PTYW638TR39',
+    vnpayHost: 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html',
+
+    // Cấu hình tùy chọn
+    testMode: true,
+    hashAlgorithm: 'SHA512',
+    enableLog: true,
+    loggerFn: ignoreLogger,
+});
+export default vnpay;
