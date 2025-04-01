cd7c8846666b1f9a976637d5ec47d236311f55fd
chison2707 <chiphansonzz17@gmail.com>
2025-04-01 10:16:21 +0700

    phân quyền admin account

--- api/v1/controllers/admin/account.controller.js
+++ api/v1/controllers/admin/account.controller.js
@@ -6,72 +6,88 @@ const paginationHelper = require("../../helper/pagination");
 
 // [GET]/api/v1/admin/accounts
 module.exports.index = async (req, res) => {
-    let find = { deleted: false };
+    const permissions = req.roles.permissions;
+    if (!permissions.includes("account_view")) {
+        return res.json({
+            code: 400,
+            message: "Bạn không có quyền xem danh sách account"
+        });
+    } else {
+        let find = { deleted: false };
 
-    if (req.query.status) {
-        find.status = req.query.status;
-    };
+        if (req.query.status) {
+            find.status = req.query.status;
+        };
 
-    // sort
-    const sort = {};
-    if (req.query.sortKey && req.query.sortValue) {
-        sort[req.query.sortKey] = req.query.sortValue;
+        // sort
+        const sort = {};
+        if (req.query.sortKey && req.query.sortValue) {
+            sort[req.query.sortKey] = req.query.sortValue;
+        }
+
+        // pagination
+        const countRecords = await Account.countDocuments(find);
+        let objPagination = paginationHelper(
+            {
+                currentPage: 1,
+                limitItems: 5
+            },
+            req.query,
+            countRecords
+        );
+        // end pagination
+
+        const accounts = await Account.find().sort(sort).limit(objPagination.limitItems).skip(objPagination.skip).select("-password");
+
+        res.json(accounts);
     }
-
-    // pagination
-    const countRecords = await Account.countDocuments(find);
-    let objPagination = paginationHelper(
-        {
-            currentPage: 1,
-            limitItems: 5
-        },
-        req.query,
-        countRecords
-    );
-    // end pagination
-
-    const accounts = await Account.find().sort(sort).limit(objPagination.limitItems).skip(objPagination.skip).select("-password");
-
-    res.json(accounts);
 };
 
 // [POST]/api/v1/admin/accounts/create
 module.exports.createPost = async (req, res) => {
-    try {
-        const emailExist = await Account.findOne({
-            email: req.body.email,
-            deleted: false
+    const permissions = req.roles.permissions;
+    if (!permissions.includes("account_create")) {
+        return res.json({
+            code: 400,
+            message: "Bạn không có quyền tạo tài khoản"
         });
-        if (emailExist) {
-            return res.json({
-                code: 400,
-                message: "Email đã tồn tại"
-            })
+    } else {
+        try {
+            const emailExist = await Account.findOne({
+                email: req.body.email,
+                deleted: false
+            });
+            if (emailExist) {
+                return res.json({
+                    code: 400,
+                    message: "Email đã tồn tại"
+                })
+            }
+
+            const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+
+            const account = new Account({
+                fullName: req.body.fullName,
+                email: req.body.email,
+                password: hashedPassword,
+                token: generate.generateRandomString(20),
+                phone: req.body.phone,
+                avatar: req.body.avatar,
+                role_id: req.body.role_id,
+                status: req.body.status,
+            });
+            const data = await account.save();
+            res.json({
+                code: 200,
+                message: "Tạo thành công",
+                data: data
+            });
+        } catch (error) {
+            res.json({
+                code: 404,
+                message: "Không tồn tại!" + error
+            });
         }
-
-        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
-
-        const account = new Account({
-            fullName: req.body.fullName,
-            email: req.body.email,
-            password: hashedPassword,
-            token: generate.generateRandomString(20),
-            phone: req.body.phone,
-            avatar: req.body.avatar,
-            role_id: req.body.role_id,
-            status: req.body.status,
-        });
-        const data = await account.save();
-        res.json({
-            code: 200,
-            message: "Tạo thành công",
-            data: data
-        });
-    } catch (error) {
-        res.json({
-            code: 404,
-            message: "Không tồn tại!" + error
-        });
     }
 };
 
@@ -113,12 +129,20 @@ module.exports.login = async (req, res) => {
 
 // [GET]/api/v1/admin/accounts/detail
 module.exports.detail = async (req, res) => {
-    const id = req.params.id;
-    const account = await Account.findOne({
-        _id: id,
-        deleted: false
-    }).select("-password");
-    res.json(account);
+    const permissions = req.roles.permissions;
+    if (!permissions.includes("account_view")) {
+        return res.json({
+            code: 400,
+            message: "Bạn không có quyền xem chi tiết tài khoản"
+        });
+    } else {
+        const id = req.params.id;
+        const account = await Account.findOne({
+            _id: id,
+            deleted: false
+        }).select("-password");
+        res.json(account);
+    }
 };
 
 // [GET]/api/v1/admin/accounts/logout
@@ -132,72 +156,96 @@ module.exports.logout = async (req, res) => {
 
 // [PATCH]/api/v1/admin/accounts/edit/:id
 module.exports.edit = async (req, res) => {
-    const id = req.params.id;
-
-    const emailExists = await Account.findOne({
-        _id: { $ne: id },
-        email: req.body.email,
-        deleted: false
-    });
-
-    if (emailExists) {
-        return res.jso({
+    const permissions = req.roles.permissions;
+    if (!permissions.includes("account_edit")) {
+        return res.json({
             code: 400,
-            message: "Email đã tồn tại"
+            message: "Bạn không có quyền chỉnh sửa tài khoản"
         });
     } else {
-        if (req.body.password) {
-            const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
-            req.body.password = hashedPassword;
+        const id = req.params.id;
+
+        const emailExists = await Account.findOne({
+            _id: { $ne: id },
+            email: req.body.email,
+            deleted: false
+        });
+
+        if (emailExists) {
+            return res.jso({
+                code: 400,
+                message: "Email đã tồn tại"
+            });
         } else {
-            delete req.body.password;
+            if (req.body.password) {
+                const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+                req.body.password = hashedPassword;
+            } else {
+                delete req.body.password;
+            }
+
+            await Account.updateOne({
+                _id: id
+            }, req.body);
+
+            res.json({
+                code: 200,
+                message: "Cập nhật tài khoản thành công"
+            })
         }
-
-        await Account.updateOne({
-            _id: id
-        }, req.body);
-
-        res.json({
-            code: 200,
-            message: "Cập nhật tài khoản thành công"
-        })
     }
 }
 
 // [PATCH]/api/v1/admin/accounts/changeStatus/:status/:id
 module.exports.changeStatus = async (req, res) => {
-    const status = req.params.status;
-    const id = req.params.id;
-
-    await Account.updateOne({
-        _id: id
-    }, {
-        status: status
-    });
-
-    res.json({
-        code: 200,
-        message: "Cập nhật trạng thái tài khoản thành công"
-    });
-}
-
-// [DELETE]/api/v1/admin/accounts/delete/:id
-module.exports.delete = async (req, res) => {
-    try {
+    const permissions = req.roles.permissions;
+    if (!permissions.includes("account_edit")) {
+        return res.json({
+            code: 400,
+            message: "Bạn không có quyền chỉnh sửa trạng thái tài khoản"
+        });
+    } else {
+        const status = req.params.status;
         const id = req.params.id;
 
-        await Account.deleteOne({
+        await Account.updateOne({
             _id: id
+        }, {
+            status: status
         });
 
         res.json({
             code: 200,
-            message: "Xóa tài khoản thành công"
+            message: "Cập nhật trạng thái tài khoản thành công"
         });
-    } catch (error) {
-        res.json({
-            code: 404,
-            message: "Không tồn tại!"
+    }
+}
+
+// [DELETE]/api/v1/admin/accounts/delete/:id
+module.exports.delete = async (req, res) => {
+    const permissions = req.roles.permissions;
+    if (!permissions.includes("account_delete")) {
+        return res.json({
+            code: 400,
+            message: "Bạn không có quyền xóa tài khoản"
         });
+    } else {
+        try {
+            const id = req.params.id;
+
+            await Account.deleteOne({
+                _id: id
+            });
+
+            res.json({
+                code: 200,
+                message: "Xóa tài khoản thành công"
+            });
+        } catch (error) {
+            res.json({
+                code: 404,
+                message: "Không tồn tại!"
+            });
+        }
     }
 }
