5acfa35b8709658af8fe6bc55f902521730b644c
chison2707 <chiphansonzz17@gmail.com>
2025-04-01 16:11:36 +0700

    phân quyền setting

--- api/v1/controllers/admin/setting.controller.js
+++ api/v1/controllers/admin/setting.controller.js
@@ -2,25 +2,33 @@ const SettingGeneral = require("../../models/settings-general.model");
 
 //[PATCH] /api/v1/admin/settings/general
 module.exports.generalPatch = async (req, res) => {
-    try {
-        const settingGeneral = await SettingGeneral.findOne({});
+    const permissions = req.roles.permissions;
+    if (!permissions.includes("general")) {
+        return res.json({
+            code: 400,
+            message: "Bạn không có quyền setting cài đặt chung"
+        });
+    } else {
+        try {
+            const settingGeneral = await SettingGeneral.findOne({});
 
-        if (settingGeneral) {
-            await SettingGeneral.updateOne({
-                _id: settingGeneral._id
-            }, req.body)
-        } else {
-            const record = new SettingGeneral(req.body);
-            await record.save();
+            if (settingGeneral) {
+                await SettingGeneral.updateOne({
+                    _id: settingGeneral._id
+                }, req.body)
+            } else {
+                const record = new SettingGeneral(req.body);
+                await record.save();
+            }
+            res.json({
+                code: 200,
+                message: "Cập nhật thành công!",
+            });
+        } catch (error) {
+            res.json({
+                code: 500,
+                message: "Lỗi" + error
+            });
         }
-        res.json({
-            code: 200,
-            message: "Cập nhật thành công!",
-        });
-    } catch (error) {
-        res.json({
-            code: 500,
-            message: "Lỗi" + error
-        });
     }
 }
