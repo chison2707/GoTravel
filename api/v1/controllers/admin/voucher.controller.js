8261abd24bfce596f6cb3e439061284fdfb1549d
chison2707 <chiphansonzz17@gmail.com>
2025-04-01 16:15:06 +0700

    phân quyền admin voucher

--- api/v1/controllers/admin/voucher.controller.js
+++ api/v1/controllers/admin/voucher.controller.js
@@ -3,121 +3,161 @@ const paginationHelper = require("../../helper/pagination");
 
 // [GET]/api/v1/admin/vouchers
 module.exports.index = async (req, res) => {
-    let find = { deleted: false };
+    const permissions = req.roles.permissions;
+    if (!permissions.includes("voucher_view")) {
+        return res.json({
+            code: 400,
+            message: "Bạn không có quyền xem danh sách voucher"
+        });
+    } else {
+        let find = { deleted: false };
 
-    // sort
-    const sort = {};
-    if (req.query.sortKey && req.query.sortValue) {
-        sort[req.query.sortKey] = req.query.sortValue;
+        // sort
+        const sort = {};
+        if (req.query.sortKey && req.query.sortValue) {
+            sort[req.query.sortKey] = req.query.sortValue;
+        }
+
+        // pagination
+        const countRecords = await Voucher.countDocuments(find);
+        let objPagination = paginationHelper(
+            {
+                currentPage: 1,
+                limitItems: 5
+            },
+            req.query,
+            countRecords
+        );
+        // end pagination
+
+        const vouchers = await Voucher.find().sort(sort).limit(objPagination.limitItems).skip(objPagination.skip);
+
+        res.json(vouchers);
     }
-
-    // pagination
-    const countRecords = await Voucher.countDocuments(find);
-    let objPagination = paginationHelper(
-        {
-            currentPage: 1,
-            limitItems: 5
-        },
-        req.query,
-        countRecords
-    );
-    // end pagination
-
-    const vouchers = await Voucher.find().sort(sort).limit(objPagination.limitItems).skip(objPagination.skip);
-
-    res.json(vouchers);
 };
 
 // [POST]/api/v1/admin/vouchers/create
 module.exports.createPost = async (req, res) => {
-    try {
-        const startDate = req.body.startDate ? new Date(req.body.startDate) : Date.now();
-        const endDate = new Date(req.body.endDate);
-
-        // Tạo đối tượng voucher mới
-        const voucher = new Voucher({
-            title: req.body.title,
-            code: req.body.code,
-            description: req.body.description,
-            quantity: req.body.quantity,
-            discount: req.body.discount,
-            startDate: startDate,
-            endDate: endDate,
+    const permissions = req.roles.permissions;
+    if (!permissions.includes("voucher_create")) {
+        return res.json({
+            code: 400,
+            message: "Bạn không có quyền tạo mới voucher"
         });
+    } else {
+        try {
+            const startDate = req.body.startDate ? new Date(req.body.startDate) : Date.now();
+            const endDate = new Date(req.body.endDate);
 
-        // Lưu voucher vào cơ sở dữ liệu
-        const data = await voucher.save();
+            // Tạo đối tượng voucher mới
+            const voucher = new Voucher({
+                title: req.body.title,
+                code: req.body.code,
+                description: req.body.description,
+                quantity: req.body.quantity,
+                discount: req.body.discount,
+                startDate: startDate,
+                endDate: endDate,
+            });
 
-        res.json({
-            code: 200,
-            message: "Tạo voucher thành công",
-            data: data
-        });
-    } catch (error) {
-        res.status(500).json({
-            code: 500,
-            message: "Lỗi khi tạo voucher: " + error.message
-        });
+            // Lưu voucher vào cơ sở dữ liệu
+            const data = await voucher.save();
+
+            res.json({
+                code: 200,
+                message: "Tạo voucher thành công",
+                data: data
+            });
+        } catch (error) {
+            res.status(500).json({
+                code: 500,
+                message: "Lỗi khi tạo voucher: " + error.message
+            });
+        }
     }
 };
 
 // [PATCH]/api/v1/admin/vouchers/edit/:id
 module.exports.edit = async (req, res) => {
-    try {
-        const id = req.params.id;
-        await Voucher.updateOne({
-            _id: id
-        }, req.body);
+    const permissions = req.roles.permissions;
+    if (!permissions.includes("voucher_edit")) {
+        return res.json({
+            code: 400,
+            message: "Bạn không có quyền chỉnh sửa voucher"
+        });
+    } else {
+        try {
+            const id = req.params.id;
+            await Voucher.updateOne({
+                _id: id
+            }, req.body);
 
-        res.json({
-            code: 200,
-            message: "Chỉnh sửa voucher thành công"
-        });
-    } catch (error) {
-        res.status(500).json({
-            code: 500,
-            message: "Lỗi khi chỉnh sửa voucher: " + error.message
-        });
+            res.json({
+                code: 200,
+                message: "Chỉnh sửa voucher thành công"
+            });
+        } catch (error) {
+            res.status(500).json({
+                code: 500,
+                message: "Lỗi khi chỉnh sửa voucher: " + error.message
+            });
+        }
     }
 };
 
 // [DELETE]/api/v1/admin/vouchers/delete/:id
 module.exports.delete = async (req, res) => {
-    try {
-        const id = req.params.id;
-        await Voucher.deleteOne({
-            _id: id
+    const permissions = req.roles.permissions;
+    if (!permissions.includes("voucher_delete")) {
+        return res.json({
+            code: 400,
+            message: "Bạn không có quyền xóa voucher"
         });
+    } else {
+        try {
+            const id = req.params.id;
+            await Voucher.deleteOne({
+                _id: id
+            });
 
-        res.json({
-            code: 200,
-            message: "Xóa voucher thành công"
-        });
-    } catch (error) {
-        res.status(500).json({
-            code: 500,
-            message: "Lỗi khi xóa voucher: " + error.message
-        });
+            res.json({
+                code: 200,
+                message: "Xóa voucher thành công"
+            });
+        } catch (error) {
+            res.status(500).json({
+                code: 500,
+                message: "Lỗi khi xóa voucher: " + error.message
+            });
+        }
     }
 };
 
 // [GET]/api/v1/admin/vouchers/detail/:id
 module.exports.detail = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const data = await Voucher.findOne({
-            _id: id
+    const permissions = req.roles.permissions;
+    if (!permissions.includes("voucher_view")) {
+        return res.json({
+            code: 400,
+            message: "Bạn không có quyền xem chi tiết voucher"
         });
+    } else {
+        try {
+            const id = req.params.id;
+            const data = await Voucher.findOne({
+                _id: id
+            });
 
-        res.json({
-            code: 200,
-            message: "Thành công",
-            data: data
-        });
-    } catch (error) {
-        res.status(500).json({
-            code: 500,
-            message: "Lỗi khi xóa voucher: " + error.message
-        });
+            res.json({
+                code: 200,
+                message: "Thành công",
+                data: data
+            });
+        } catch (error) {
+            res.status(500).json({
+                code: 500,
+                message: "Lỗi khi xóa voucher: " + error.message
+            });
+        }
     }
 };
