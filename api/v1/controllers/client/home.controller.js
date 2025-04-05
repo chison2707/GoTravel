d7b21d30702205f486e76c92ad771644dbe3cf65
chison2707 <chiphansonzz17@gmail.com>
2025-04-05 20:31:12 +0700

    update home

--- api/v1/controllers/client/home.controller.js
+++ api/v1/controllers/client/home.controller.js
@@ -1,17 +1,29 @@
 const Tour = require("../../models/tour.model");
+const Hotel = require("../../models/hotel.model");
 
 // [GET]/api/v1/
 module.exports.index = async (req, res) => {
+    // top tours
     const topSallers = await Tour.find({
         status: "active",
         deleted: false
     }).sort({ sold: "desc" }).limit(5);
+
+    // new tours
     const newTours = await Tour.find({
         status: "active",
         deleted: false
     }).sort({ createdAt: -1 }).limit(5);
+
+    // top hotels
+    const topHotelSallers = await Hotel.find({
+        status: "active",
+        deleted: false
+    }).sort({ sold: "desc" }).limit(5);
+
     res.json({
         topSallers: topSallers,
-        newTours: newTours
-    })
+        newTours: newTours,
+        topHotelSallers: topHotelSallers
+    });
 };
