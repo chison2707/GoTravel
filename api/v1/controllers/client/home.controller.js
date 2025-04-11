30ac7fd4bbc3942d92e644f6b6ade8436be1767f
chison2707 <chiphansonzz17@gmail.com>
2025-04-11 11:05:58 +0700

    top star hotel

--- api/v1/controllers/client/home.controller.js
+++ api/v1/controllers/client/home.controller.js
@@ -1,5 +1,6 @@
 const Tour = require("../../models/tour.model");
 const Hotel = require("../../models/hotel.model");
+const Review = require("../../models/hotelReview.model");
 
 // [GET]/api/v1/
 module.exports.index = async (req, res) => {
@@ -21,9 +22,40 @@ module.exports.index = async (req, res) => {
         deleted: false
     }).sort({ sold: "desc" }).limit(5);
 
+    // top star hotels
+    const hotels = await Hotel.find({
+        status: "active",
+        deleted: false
+    });
+
+    const hotelWithRating = [];
+
+    for (const item of hotels) {
+        const reviews = await Review.find({ hotel_id: item._id });
+        if (reviews.length > 0) {
+            const totalRating = reviews.reduce((sum, review) => sum + review.rating, 0);
+            const averageRating = totalRating / reviews.length;
+
+            hotelWithRating.push({
+                hotel: item,
+                averageRating
+            });
+        } else {
+            hotelWithRating.push({
+                hotel: item,
+                averageRating: null
+            });
+        }
+    }
+
+    const topRatedHotels = hotelWithRating
+        .sort((a, b) => b.averageRating - a.averageRating)
+        .slice(0, 5);
+
     res.json({
         topSallers: topSallers,
         newTours: newTours,
-        topHotelSallers: topHotelSallers
+        topHotelSallers: topHotelSallers,
+        topRatedHotels: topRatedHotels
     });
 };
